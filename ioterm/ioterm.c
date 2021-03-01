2f1e03bc100a7c36f3610b4dc286919d0d3c256f
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-03-01 11:30:57 -0500

    Add rows, cols, and getch to IO::Term
    Currently, the getch has a bug where it doesn't ask for the character again when it's called a second time, but that is a bug with curly itself, not with this code.

--- ioterm/ioterm.c
+++ ioterm/ioterm.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <sys/ioctl.h>
+
 
 void __curly_prim__putch(char c)
 {
     printf("%c", c);
+}
+
+char __curly_prim__getch()
+{
+    return getchar();
+}    
+
+
+struct winsize sz;
+
+int __curly_prim__cols()
+{
+    ioctl( 0, TIOCGWINSZ, &sz );
+    return sz.ws_col;
+}
+
+int __curly_prim__rows()
+{
+    ioctl( 0, TIOCGWINSZ, &sz );
+    return sz.ws_row;
 }
